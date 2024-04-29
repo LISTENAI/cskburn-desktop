45e765002ce8887cc2b2a4b8e1f66389de9c2a85
xychen <xychen@listenai.com>
2024-04-29 14:09:11 +0800

    Use naive-ui builtin `useOsTheme` for dark mode switch
    Signed-off-by: xychen <xychen@listenai.com>

--- src/AppWindow.vue
+++ src/AppWindow.vue
@@ -6,28 +6,16 @@
 </template>
 
 <script lang="ts" setup>
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { computed } from 'vue';
 import {
   NConfigProvider,
   NGlobalStyle,
   darkTheme,
+  useOsTheme,
 } from 'naive-ui';
-import { appWindow } from '@tauri-apps/api/window';
 
 import App from '@/App.vue';
-import type { UnlistenFn } from '@tauri-apps/api/event';
 
-const darkMode = ref(false);
-
-let unlisten: UnlistenFn | undefined;
-
-onMounted(async () => {
-  unlisten = await appWindow.onThemeChanged(({ payload: theme }) => {
-    darkMode.value = theme == "dark";
-  });
-});
-
-onBeforeUnmount(() => {
-  unlisten?.();
-});
+const osTheme = useOsTheme();
+const darkMode = computed(() => osTheme.value == 'dark');
 </script>
