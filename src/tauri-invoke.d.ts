3f8fbddd21be60100293a45a38a2d69742e19cae
xychen <xychen@listenai.com>
2024-05-27 15:21:58 +0800

    Fix typing declaration for `@tauri-apps/api/core`
    * so the overrides won't overwrite other APIs exposed from the module
    
    Signed-off-by: xychen <xychen@listenai.com>

--- src/tauri-invoke.d.ts
+++ src/tauri-invoke.d.ts
@@ -1,8 +1,8 @@
+import '@tauri-apps/api/core';
+
 declare module '@tauri-apps/api/core' {
-  import { invoke, type InvokeArgs } from '@tauri-apps/api/core';
   import type { ISection } from '@/utils/readHex';
   import type { IPartition } from '@/utils/images';
-  function invoke<T>(cmd: string, args?: InvokeArgs): Promise<T>;
   function invoke(cmd: 'read_hex', args: { path: string }): Promise<ISection[]>;
   function invoke(cmd: 'read_lpk', args: { path: string }): Promise<IPartition[]>;
   function invoke(cmd: 'list_ports'): Promise<string[]>;
