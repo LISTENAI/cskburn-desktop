7bc897cdfcec4982d73f3425c8106d8545c7adee
xychen <xychen@listenai.com>
2025-11-26 11:15:20 +0800

    Refactor LogsView
    In a more natural form
    
    Signed-off-by: xychen <xychen@listenai.com>

--- src/components/sections/LogView.vue
+++ src/components/sections/LogView.vue
@@ -1,26 +1,33 @@
 <template>
   <n-element>
-    <n-input ref="el" type="textarea" :value="props.logs" placeholder="没有日志" readonly :resizable="false"
+    <n-input ref="logs" type="textarea" :value="props.logs" placeholder="没有日志" readonly :resizable="false"
       :style="{ height: '100%', fontFamily: 'var(--font-family-mono)' }" />
   </n-element>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, useTemplateRef } from 'vue';
+import { watchThrottled } from '@vueuse/core';
 import { NElement, NInput, type InputInst } from 'naive-ui';
-import { nextTick, ref, watch } from 'vue';
 
 const props = defineProps<{
   logs: string;
 }>();
 
-const el = ref<InputInst | null>(null);
+const logsEl = useTemplateRef<InputInst>('logs');
 
-watch(props, () => {
-  nextTick(() => {
-    el.value?.scrollTo({
-      behavior: 'instant',
-      top: el.value.textareaElRef?.scrollHeight ?? 0,
-    });
+function scrollToBottom() {
+  logsEl.value?.scrollTo({
+    behavior: 'instant',
+    top: logsEl.value.textareaElRef?.scrollHeight ?? 0,
   });
-}, { immediate: true });
+}
+
+watchThrottled(() => props.logs, () => {
+  scrollToBottom();
+}, { immediate: true, throttle: 100, trailing: true });
+
+onMounted(() => {
+  scrollToBottom();
+});
 </script>
